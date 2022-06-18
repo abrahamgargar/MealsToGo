f20478e53101964ed9e7951a6f9c8ef3c750422c
abe <abraham.garcia@vistair.com>
2022-06-18 12:03:31 +0100

    feature restaurant search bar

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text, Image, View } from "react-native";
+import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Orry Alexander Garcia",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3tiNOO4g8aA0JWZgzNiVFgXXBuoEzQBHdxQ&usqp=CAU",
+    ],
+    address = "2 Robbins Close. Bristol",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+      <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                BOOKED
+                {/* CLOSED TEMPORARILY */}
+              </Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            {/* <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer> */}
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
